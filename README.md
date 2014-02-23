7f778e62f6e09c5a50a75c002a03b40b96548639
MG <885ce300@opayq.com>
2014-02-24 04:18:54 +0530

    Added partition using different season folders

--- README.md
+++ README.md
@@ -18,6 +18,20 @@ TVShows/
 		folder1/
 		file
 
+Final Structure Of directories
+TVShows/
+	Show1/
+		season 1/
+			1x01 - episode title
+			1x02 - episode title
+		season 2/
+			2x01 - episode title
+			2x02 - episode title
+	Show2/
+		season 1/
+			1x01
+	Show3/
+		season 3
 This script converts the files of names
 [720pMkv.Com]_doctor.who.2005.s02e04.480p.BluRay.x264-GAnGSteR.mkv
 to
@@ -25,5 +39,8 @@ Doctor Who (2005)-02x04-The Girl in the Fireplace.mp4
 
 For this script to work 
 1. Edit the allShowDir variable to point to your TV Show Directory
-2. Make Sure that the files have the format SxxExx or xxXxx 
+2. Make Sure that the files have the format S01E03 or 01x03 (SxxExx or xxXxx) 
 to recognise their season number and episode number values.
+
+This script also stores a log file for all the renames in the directory
+so that you can revert manually the renames that turn out to be wrong
--- main.py
+++ main.py
@@ -33,19 +33,12 @@ def findShowId(showName):
 			return showid
 	return -1
 def seasonEpisode(fileName):
-	result = re.search('.*([sS]{1}([0-9]+)[eE]{1}([0-9]+)).*',fileName);
+	result = re.search('[.]*([sS]{0,1}([0-9]+)[eExX]{1}([0-9]+)).*',fileName);
 	if result:
 		found = result.group(1)
 		ret = []
 		ret.append(int(result.group(2)))
 		ret.append(int(result.group(3)))
-		return ret
-	result = re.search('.*(([0-9]+)[xX]{1}([0-9]+)).*',fileName);
-	if result:
-		found = result.group(1)
-		ret = []
-		ret.append(result.group(2))
-		ret.append(result.group(3))
 		return ret
 	return -2
 
@@ -56,14 +49,17 @@ def newName(showid,season,episode):
 	xml = openResource(epInfoUrl)
 	root = ET.fromstring(xml)
 	ret = root.find('name').text
+	newShowName = root.find('name').text
 	if len(root.findall('episode')) == 1:
 		for episode in root.findall('episode'):
 			ret = ret + "-" + episode.find('number').text
 			ret = ret + '-' + episode.find('title').text
-		return ret
-	return -3
+		return ret, newShowName
+	return -3,""
 if __name__ == '__main__':        
-	allShowsDir = ""
+	allShowsDir = "C:/Users/Mohit Gahlot/Videos/TV Shows"
+	f = open(allShowsDir+'/log.txt','w');
+	renVal = 0
 	#Edit this variable to point to you Tv shows Directory
 	showNameList = [x for x in os.listdir(allShowsDir)]
 	for show in showNameList:
@@ -75,14 +71,52 @@ if __name__ == '__main__':
 		for root,dirs,files in os.walk(showDir):
 			for fyle in files:
 				temp = seasonEpisode(fyle)
+				if temp != -2:
+					print("For", fyle ,temp[0],temp[1])
 				if temp == -2:
 					print("file: "+fyle+' in show: '+show+" doesn't seem to match to any episode")
 					print("Please check its name contains format SxxExx or xxXxx format")
 					continue
-				print("Old Name:"+fyle)
-				newFyle = newName(showid,temp[0],temp[1])
+				
+				newFyle,newShowName = newName(showid,temp[0],temp[1])
+				
 				if newFyle == -3:
 					print("Sorry Couldn't file the correct episode for show: "+show+' file: '+fyle)
 					continue
-				newFyle = newFyle + "." + fyle.split('.')[-1]
-				print("New Name:"+newFyle)
+				newFyleExtension = "." + fyle.split('.')[-1]
+				
+				
+				if not os.path.exists(allShowsDir+'/'+newShowName):
+					print("Folder for show "+newShowName+ " not found, Creating one now..")
+					os.makedirs(allShowsDir+'/'+newShowName)
+				
+				if not os.path.exists(allShowsDir+'/'+newShowName+'/season '+str(temp[0])):
+					print("Folder for season "+str(temp[0])+ " not found in show"+ newShowName+" , Creating one now..")
+					os.makedirs(allShowsDir+'/'+newShowName+'/season '+str(temp[0]))
+				oldpath = (root+'/'+fyle).replace("\\",'/')
+				newpath = allShowsDir+'/'+newShowName+'/season '+str(temp[0])+'/'+newFyle+newFyleExtension;
+
+				if oldpath.lower() == newpath.lower():
+					continue
+				if os.path.isfile(allShowsDir+'/'+newShowName+'/season '+str(temp[0])+'/'+newFyle+newFyleExtension):
+					i = 1;
+					while os.path.isfile(allShowsDir+'/'+newShowName+'/season '+str(temp[0])+'/'+newFyle+"-("+str(i)+")"+newFyleExtension):
+						i = i + 1
+					try:
+						os.rename(root+'/'+fyle,allShowsDir+'/'+newShowName+'/season '+str(temp[0])+'/'+newFyle+"-("+str(i)+")"+newFyleExtension)	
+						print("Old Name:"+fyle)
+						print("New Name:"+newFyle+"-("+str(i)+")"+newFyleExtension)
+					except Exception:
+						print("Something went wrong during renaming");
+					f.write(str(renVal)+'\n'+root+'/'+fyle+'\n'+allShowsDir+'/'+newShowName+'/season '+str(temp[0])+'/'+newFyle+"-("+str(i)+")"+newFyleExtension+'\n')
+				else:
+					try:
+						os.rename(root+'/'+fyle,allShowsDir+'/'+newShowName+'/season '+str(temp[0])+'/'+newFyle+newFyleExtension)
+						print("Old Name:"+fyle)
+						print("New Name:"+newFyle+newFyleExtension)
+					except Exception:
+						print("Something went wrong during renaming");
+					f.write(str(renVal)+'\n'+root+'/'+fyle+'\n'+allShowsDir+'/'+newShowName+'/season '+str(temp[0])+'/'+newFyle+newFyleExtension+'\n')
+				renVal = renVal + 1
+	f.close()
+	print(" Renaming Completed Log saved to"+allShowsDir)
